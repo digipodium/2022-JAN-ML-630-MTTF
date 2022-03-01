1476917825867d9177b1bbba2efdff2381a71f46
zaid kamil <xaidmetamorphos@gmail.com>
2022-03-01 20:04:30 +0530

    done

--- app_1/app.py
+++ app_1/app.py
@@ -3,12 +3,24 @@ from statistics import mode
 from flask import Flask, render_template,request
 import numpy as np
 from joblib import load
+import os
 
 app = Flask(__name__)
 
 def load_clf_model():
+	print(os.listdir())
 	filepath = 'app_1/clf_ap.pkl'
 	return load(filepath)
+
+def predict(age=0,salary=0):
+	userinp = np.array([[age,salary]]) 
+	model_dict = load_clf_model()
+	x = model_dict.get('scaler').transform(userinp)
+	p = model_dict.get('classifier').predict(x)
+	if p[0] == 0:
+		return "will not purchase"
+	else:
+		return "will make purchase"
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -16,14 +28,8 @@ def index():
 		form = request.form
 		age = int(form.get('age'))
 		salary = float(form.get('salary'))
-		userinp = np.array([[age,salary]]) 
-		model_dict = load_clf_model()
-		
-		x = model_dict.get('scaler').transform(userinp)
-		print(x)
-		p = model_dict.get('classifier').predict(x)
-		print(p) # will be a single item array
-  
+		result = predict(age,salary)
+		return render_template('index.html',age=age, salary=salary, result=result)
 	return render_template('index.html')
 
 if __name__ == '__main__':
