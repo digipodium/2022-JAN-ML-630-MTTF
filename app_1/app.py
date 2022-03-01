b5e28860833a00abd66db088008e5612b01deb99
zaid kamil <xaidmetamorphos@gmail.com>
2022-03-01 15:30:14 +0530

    Update app.py

--- app_1/app.py
+++ app_1/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def index():
     form = request.form
-    print(form)
+    print(form.get('age'))
     return render_template('index.html')
 
 if __name__ == '__main__':
