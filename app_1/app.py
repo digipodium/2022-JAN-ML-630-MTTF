26386f62ed8716efa7251e577d4c2a57c83ccf34
zaid kamil <xaidmetamorphos@gmail.com>
2022-03-24 19:39:27 +0530

    app database

--- app_1/app.py
+++ app_1/app.py
@@ -1,15 +1,36 @@
-# fapp -> shortcut from happy flasker extension
+
 from statistics import mode
 from flask import Flask, render_template,request
+from flask_sqlalchemy import SQLAlchemy # step 1
 import numpy as np
 from joblib import load
 import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///predictions.sqlite3'  # step 2
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#  database class
+db = SQLAlchemy(app) # step 3
+
+# step 4 create a table using python class
+class PredHistory(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	age = db.Column(db.Integer)
+	salary = db.Column(db.Float)
+	prediction = db.Column(db.String)
+
+	def __str__(self):
+		return f"{self.id}, {self.age}, {self.salary}, {self.prediction}"
+
+
+#  end of database class
+# step 5 is on terminal
+
 
 def load_clf_model():
 	print(os.listdir())
-	filepath = 'app_1/clf_ap.pkl'
+	filepath = 'clf_ap.pkl'
 	return load(filepath)
 
 def predict(age=0,salary=0):
@@ -24,13 +45,20 @@ def predict(age=0,salary=0):
 
 @app.route('/', methods=['GET','POST'])
 def index():
+
 	if request.method=="POST":
 		form = request.form
 		age = int(form.get('age'))
 		salary = float(form.get('salary'))
 		result = predict(age,salary)
-		return render_template('index.html',age=age, salary=salary, result=result)
-	return render_template('index.html')
+		record = PredHistory(age=age,salary=salary,prediction=result) # step 6
+		db.session.add(record)
+		db.session.commit()
+		history = PredHistory.query.all()
+		return render_template('index.html',age=age, salary=salary, result=result, history=history)
+	else:
+		history = PredHistory.query.all()
+	return render_template('index.html',history=history)
 
 if __name__ == '__main__':
 	app.run(debug=True)
